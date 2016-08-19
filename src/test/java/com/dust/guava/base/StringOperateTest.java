32636ae7531715da8ed43b69cb6039fde0df4323
lautayfir@163.com <Sr.1autayfir>
2016-08-19 12:52:11 +0800

    add

--- src/test/java/com/dust/guava/base/StringOperateTest.java
+++ src/test/java/com/dust/guava/base/StringOperateTest.java
@@ -1,8 +1,6 @@
 package com.dust.guava.base;
 
-import com.google.common.base.CharMatcher;
-import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
+import com.google.common.base.*;
 import org.junit.Test;
 
 import java.util.List;
@@ -45,11 +43,31 @@ public class StringOperateTest {
     @Test
     public void CharMatcherTest(){
         String uuidStr = UUID.randomUUID().toString();
-        String removeFrom = CharMatcher.JAVA_ISO_CONTROL.removeFrom("xxx"); //移除control字符
-        String retainFrom = CharMatcher.DIGIT.retainFrom("asdsad11de2234550");  //只保留数字字符
+        byte[] bytes = uuidStr.getBytes(Charsets.UTF_8);
 
+        String noContro = CharMatcher.JAVA_ISO_CONTROL.removeFrom(uuidStr); //移除字符串中的control字符
+        String spaced = CharMatcher.WHITESPACE.trimAndCollapseFrom(uuidStr, ' '); //去除两端空格,并把中间的连续空格替换成单个空格
+        String onlyDigit = CharMatcher.DIGIT.retainFrom(uuidStr);  //字符串中只保留数字字符
+        String digitAndLower = CharMatcher.JAVA_DIGIT.or(CharMatcher.JAVA_LOWER_CASE).retainFrom(uuidStr);  //只保留数字以及小写字母
+        String noDigit = CharMatcher.JAVA_DIGIT.replaceFrom(uuidStr, "*");  //用*替代所有数字
+
+        System.out.println(noContro);
+        System.out.println(spaced);
+        System.out.println(onlyDigit);
+        System.out.println(digitAndLower);
+        System.out.println(noDigit);
     }
 
+
+    @Test
+    public void CaseFormatTest(){
+        String name = "user_name";
+        String toLowerCamel = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, name);//下划线小写词组转换为小写驼峰
+        String toUpperCamel = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, name);//下划线小写词组转换为小写驼峰
+        System.out.println(toLowerCamel);
+        System.out.println(toUpperCamel);
+
+    }
 
 
 }
